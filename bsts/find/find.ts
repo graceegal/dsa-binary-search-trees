9522b5047297d264615e154c248919815130dc33
Jazz Cheema <jaskamalc@gmail.com>
2024-04-24 16:47:42 -0700

    finished find

--- bsts/find/find.ts
+++ bsts/find/find.ts
@@ -1,18 +1,34 @@
 import { BNodeNum } from "../common/bst";
-
+import { StackNum } from "../common/stack";
 
 /** findRecursively(val): Search from the invoking node for a node with value val.
  * Returns the node, if found; else null */
 
 function findRecursively(node: BNodeNum | null, val: number): BNodeNum | null {
-  return null;
-}
+  if (!node) return null;
+  let current = node;
 
+  if (current!.val === val) return current;
+
+  if (val < current!.val) {
+    return findRecursively(current.left, val);
+  } else {
+    return findRecursively(current.right, val);
+  }
+
+}
 
 /** find(val): Search the BST for a node with value val.
  * Returns the node, if found; else null. */
 
 function find(node: BNodeNum | null, val: number): BNodeNum | null {
+  let current = node;
+
+  while (current) {
+    if (current.val === val) return current;
+
+    current = val < current.val ? current.left : current.right;
+  }
   return null;
 }
 
